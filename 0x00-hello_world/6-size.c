c7a3ff028d030c5fce2a2deb698d2ebcfa1e49f2
grayolaRSA <gtndanga@gmail.com>
2022-12-09 13:10:48 -0800

    create program to get sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %2d bytes \n", sizeof(char));
+printf("Size of an int: %2d bytes \n", sizeof(short int));
+printf("Size of a long int: %2d bytes \n", sizeof(long int));
+printf("Size of a long long int: %2d bytes \n", sizeof(long double));
+return (0);
+}
