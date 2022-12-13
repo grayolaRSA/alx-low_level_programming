77ad25ce156a867d220156bfb221983d285b5251
grayolaRSA <gtndanga@gmail.com>
2022-12-12 17:42:35 -0800

    formatted headers according to betty format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,14 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* Prints a random number and states whether
- * it is positive, negative, or zero.
- * betty style doc for function main goes there */
+/** Prints a random number and states whethe */
+/* it is positive, negative, or zero. */
+
+/**
+ * main - betty style doc for function main goes there
+ *
+ * Return: Always 0.
+*/
 
 int main(void)
 {
