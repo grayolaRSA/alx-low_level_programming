cd3c435d14cd8b3994d42bc309e0302a82a3c815
grayolaRSA <gtndanga@gmail.com>
2022-12-14 09:07:06 -0800

    add nine times table script

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,41 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *times_table - function prototype
+ */
 void times_table(void);
+
 /**
  * main - produces results of nine times table
  *
- * Return: Always 0.
+ * Return: Always int.
  */
 int main(void)
 
 {
+	int i;
 
-	int i = 0;
-	int j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		int j;
 
-	times_table = i * j;
 
-	while (i < 9)
+		for (j = 0; j < 10; j++)
 		{
+			int m;
 
-			while (j < 9)
+			m = i * j;
 
-				{
-					printf("%p  ", times_table);
-					j++;
-				}
-
-			printf("\n");
-			i++;
-
+				printf("%d ", m);
 		}
+		printf("\n");
+	}
 	return (0);
+}
+
+void times_table(void)
+
+{
+
+	printf("Sing hallelujah");
 }
