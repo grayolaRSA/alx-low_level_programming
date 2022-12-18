0ffad814648a7a39ec81af90efd314eaa8d9f040
grayolaRSA <gtndanga@gmail.com>
2022-12-17 23:54:35 -0800

    add remaining mandatory files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,27 @@
-#include "main.h"
+# include "main.h"
 
 /**
- *print_sign - prints the sign of the number
+ * print_sign - prints sign of number
  *
- *Return: +1, 0 or -1
-/*
+ * Return: Always 1, 0 0r -1.
+ */
 
 int print_sign(int n)
-
 {
-int r;
-int n;
+	int r;
 
-if (n > 0)
-{
-r = 1;
-printf('+');
-}
-
-else if (n < 0)
-{
-r = -1;
-printf('-');
-}
-
-else
-{
-r = 0;
-printf('0');
-}
-return (r);
+	if (n > 0)
+	{
+		_putchar('+');
+		r = 1;
+	} else if (n == 0)
+	{
+		_putchar('0');
+		r = 0;
+	} else
+	{
+		_putchar('-');
+		r = -1;
+	}
+	return (r);
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,24 +3,25 @@
 
 /**
  *_abs - calculates the absolute value of an integer
+ * @n: describe
  *
- *Return - always 0
-/*
+ *Return: always 0
+ */
 
 
 int _abs(int n)
 
 {
 
-if (n >= 0)
+	if (n >= 0)
 
-{
-return (n);
-}
+	{
+		return (n);
+	}
 
-else
-{
-return (-n);
-}
-return (0);
+	else
+	{
+		return (-n);
+	}
+	return (0);
 }
