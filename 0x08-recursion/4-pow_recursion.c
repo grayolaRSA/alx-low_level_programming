203040d3c6d29e4c9edc95c30b50f138fdad9732
grayolaRSA <gtndanga@gmail.com>
2023-01-05 23:03:16 +0200

    recursive functions

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_pow_recursion - raises number x to power y
+*@x: first integer
+*@y: power to be raised
+*Return: returns integer
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+
+return (-1);
+
+if (y == 0)
+
+return (1);
+
+if (y == 1)
+
+return (x);
+
+else
+
+return (x * (_pow_recursion(x, y - 1)));
+
+}
