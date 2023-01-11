b135be5a0bb491682d95e5d7c531f4b89ac7d79c
grayolaRSA <gtndanga@gmail.com>
2023-01-11 21:52:13 +0200

    rewrite concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
 *str_concat - concatenates two strings
 *@s1: first string
@@ -12,40 +10,32 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s;
-unsigned int y;
-unsigned int len1;
-unsigned int len2;
+unsigned int y, i, j, k;
 
 if (s1 == NULL)
+i = 0;
+for (i = 0; s1[i] != '\0'; i++)
+	;
 
-{
-s1 = "";
-}
 
 if (s2 == NULL)
-{
-s2 = "";
-}
+k = 0;
+for (k = 0; s2[k] != '\0'; k++)
+	;
 
-{
-len1 = strlen(s1);
-len2 = strlen(s2);
-s = malloc((len1 + len2 + 1) * sizeof(char));
-}
-printf("the address of the new memory is %p\n", (void *)s);
-printf("the size of the new memory is %lu\n", sizeof(s));
+j = (i + k + 1);
+s = malloc(j * (sizeof(char)));
+
 if (s == NULL)
 return (NULL);
 
-for (y = 0; y < len1; y++)
-{
+for (y = 0; y < i; y++)
 s[y] = s1[y];
-}
 
-for (y = 0; y < len2; y++)
-{
-s[len1 + y] = s2[y];
-}
+for (y = 0; y < k; y++)
+s[i + y] = s2[y];
+
+s[i + k] = '\0';
 
 return (s);
 }
