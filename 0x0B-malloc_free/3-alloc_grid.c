0f039b0ce16ef9187f865ddc70477eb0bc3ef556
grayolaRSA <gtndanga@gmail.com>
2023-01-11 23:25:42 +0200

    trying to free up memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,21 +22,17 @@ if (grid == NULL)
 return (NULL);
 
 for (i = 0; i < height; i++)
-{
 grid[i] = calloc(width, sizeof(int));
 
 if (grid[i] == NULL)
-{
 for (j = 0; j < i; j++)
 free(grid[j]);
 return (NULL);
-}
-{
+
 for (j = 0; j < width; j++)
 grid[i][j] = 0;
-}
 free(grid);
-}
+
 return (grid);
 
 }
