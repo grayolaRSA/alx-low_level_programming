95b46c91c628175be8666855fae5b3feefbb8c92
grayolaRSA <gtndanga@gmail.com>
2023-01-11 22:58:23 +0200

    add 2D arrays file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*alloc_grid - allocates grid
+*@width: int
+*@height: int
+*Return: 2 dimensional array
+*/
+
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **grid;
+
+if (width == 0 || height == 0)
+return (NULL);
+
+grid = malloc(height * sizeof(int *));
+
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+grid[i] = calloc(width, sizeof(int));
+
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+return (NULL);
+}
+{
+for (j = 0; j < width; j++)
+grid[i][j] = 0;
+}
+}
+return (grid);
+}
