ae36fed1552a923516f0050591e0d1105bc78e6c
grayolaRSA <gtndanga@gmail.com>
2023-01-14 16:49:59 +0200

    add update to concat file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
 char *s;
-unsigned int y, i, j, k, l;
+unsigned int y, i, k;
 
 if (s1 == NULL)
 i = 0;
@@ -28,27 +28,25 @@ else
 for (k = 0; s2[k] != '\0'; k++)
 	;
 
-l = (i + ((n + 1) / sizeof(char)));
-s = malloc(l);
+k = 0;
+if (k < n)
+k = n;
+
+s = malloc(i + k + 1);
 
 if (s == NULL)
 return (NULL);
 
+{
 for (y = 0; y < i; y++)
 s[y] = s1[y];
-
-j = (n / (sizeof(char)));
-
-if (j < k)
-{
-for (y = 0; y < j; y++)
-s[i + y] = s2[y];
 }
-else
+
 {
 for (y = 0; y < k; y++)
 s[i + y] = s2[y];
 }
+
 s[i + k] = '\0';
 
 return (s);
