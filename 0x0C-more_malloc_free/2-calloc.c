8f15df8809a11a2a75fdb063399ff0c216052dc8
grayolaRSA <gtndanga@gmail.com>
2023-01-14 17:27:44 +0200

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array, using malloc.
- * @nmemb: number of elements in the array
- * @size: size in bytes of the elements
- *
- * Return: void pointer to allocated memory
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *p;
-	unsigned int i;
+*_calloc - allocates memory for 2D array
+*@nmemb: number of elements
+*@size: size of elements
+*Return: pointer or null
+*/
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
-	return (p);
+void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+char *p;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+
+for (i = 0; i <= (nmemb * size); i++)
+p[i] = 0;
+
+return (p);
 }
