b1ead019d5c73f42b5779681c29637d6863e99d5
grayolaRSA <gtndanga@gmail.com>
2023-01-18 20:37:07 +0200

    add new directory for function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+*print_name - prints name
+*@name: name
+*return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{}
+
+#endif
