9390c0c0c0dfb2454f447b8bebfb13a188ef1e43
grayolaRSA <gtndanga@gmail.com>
2023-01-18 21:56:07 +0200

    add first function pointer files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#include "function_pointers.h"
+#include <unistd.h>
 
 /**
 *print_name - prints name
 *@name: name
+*@f: function pointer
 *return: void
 */
 
 void print_name(char *name, void (*f)(char *))
-{}
+{
+f(name);
+}
 
-#endif
