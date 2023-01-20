ffc68b412759fb24988d964346284f77edad6f12
grayolaRSA <gtndanga@gmail.com>
2023-01-20 11:12:56 +0200

    update to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC _FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
- void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 
