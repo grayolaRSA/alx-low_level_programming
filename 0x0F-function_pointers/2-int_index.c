956c1c8b09f5cb1bf7bb5d416745e98a43bcd60e
grayolaRSA <gtndanga@gmail.com>
2023-01-23 06:58:43 +0200

    update

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+*int_index - searches for match in array
+*@array: array to be searched within
+*@size: size of array
+*@cmp: function that compares
+*Return: either index or -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+
+return (-1);
+}
