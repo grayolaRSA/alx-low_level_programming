d60fddf5487cba38b0517a15da43953b6a02d6ad
grayolaRSA <gtndanga@gmail.com>
2023-01-23 06:24:48 +0200

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+
+/**
+*array_iterator - performs an action
+*@array: integer array
+*@size: size of array
+*@action: function to be used
+*return: nothing
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+unsigned long i;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+
