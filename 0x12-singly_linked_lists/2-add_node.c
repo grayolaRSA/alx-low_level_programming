43c7b6dd216cbef6060bdc174afbd9cc92c9967b
grayolaRSA <gtndanga@gmail.com>
2023-01-31 18:41:22 +0200

    updates

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	new_node->len = strlen(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
