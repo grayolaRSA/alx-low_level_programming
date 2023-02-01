21b25ff8225cac3fba4500182257ecc44e806417
grayolaRSA <gtndanga@gmail.com>
2023-02-01 19:20:02 +0200

    add end node file

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *add_node_end - function to add node at end
+ *@head: pointer to head node
+ *@str: string in node
+ *Return: pointer to former head node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current = *head;
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+	return (new_node);
+}
