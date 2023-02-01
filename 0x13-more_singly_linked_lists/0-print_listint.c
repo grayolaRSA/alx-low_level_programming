94bbf14f57b87905ff45d42b5e26f568f19cdc01
grayolaRSA <gtndanga@gmail.com>
2023-02-01 20:44:36 +0200

    add readme and header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * print_listint  - prints singly linked list
+ *@h: list to be printed
+ *Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("[%d] \n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
