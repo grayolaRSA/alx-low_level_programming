b2200b54fa7cb073596494048519e64c894e2a42
grayolaRSA <gtndanga@gmail.com>
2023-02-02 20:59:49 +0200

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *print_binary - prints converted binary
+ *@n: input integer to be converted
+ *Return: binary output
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (8 * sizeof(unsigned long int) - 1);
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+
+	while (!(n & mask))
+	{
+		mask >>= 1;
+	}
+	while (mask)
+	{
+		putchar(n & mask ? '1' : '0');
+		mask >>= 1;
+	}
+}
