063cfe375af3dcb3ece0beb7f860b64caa64f841
grayolaRSA <gtndanga@gmail.com>
2023-02-02 22:36:38 +0200

    add first structures files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: dog owner repository structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
