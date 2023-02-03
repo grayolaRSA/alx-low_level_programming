1b81c3e29fc0d38749d9d8cab79caa996867ee2f
grayolaRSA <gtndanga@gmail.com>
2023-02-03 11:18:12 +0200

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,33 +18,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = strdup(name);
+	d->name = malloc(strlen(name) + 1);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	strcpy(d->name, name);
+
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = malloc(strlen(owner) + 1);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
+	strcpy(d->owner, owner);
 
 	return (d);
 
-}
-/**
- *free_dog - frees all memory
- *@d: struct to be freed
- *Return: void
- */
-
-void free_dog(struct dog *d)
-{
-	free(d->name);
-	free(d->owner);
-	free(d);
 }
