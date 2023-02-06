529dd7c135da14da046a216331aff64114b0d17b
grayolaRSA <gtndanga@gmail.com>
2023-02-06 22:13:58 +0200

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,13 +25,28 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		l_written = write(fd, "", 0);
 	else
-		l_written = write(fd, text_content, strlen(text_content));
+		l_written = write(fd, text_content, _strlen(text_content));
 
-	if (l_written == -1 || (size_t) l_written != strlen(text_content))
+	if (l_written == -1 || (size_t) l_written != _strlen(text_content))
 	{
 		close(fd);
 		return (-1);
 	}
 	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
+unsigned long int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _putchar(char c);
+unsigned long int _strlen(char *s);
 
 #endif
