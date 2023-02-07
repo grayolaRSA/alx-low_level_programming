8fb8850adeaf01665c450199093728d8873bd54d
grayolaRSA <gtndanga@gmail.com>
2023-02-07 22:24:29 +0200

    update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *free_listint2 - frees listint_t list
+ *@head: pointer to head node
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp2 = temp->next;
+		free(temp);
+		temp = temp2;
+	}
+	*head = NULL;
+}
