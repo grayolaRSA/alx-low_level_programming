2bdca677727380ac2a57d819287d2bd6b9a35674
grayolaRSA <gtndanga@gmail.com>
2023-02-08 22:57:28 +0200

    update

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *print_diagsums - print diagonal sums
+ *@a: int
+ * @size: int
+ *Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * size + i];
+sum2 += a[i * size + (size - i - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
