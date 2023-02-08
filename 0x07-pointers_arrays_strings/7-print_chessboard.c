118832cf3d2267e2ba4b5221423ad617da3660af
grayolaRSA <gtndanga@gmail.com>
2023-02-08 22:39:32 +0200

    update

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_chessboard - prints chessboard
+ *@a: box pointer
+ *Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c ", a[i][j]);
+}
+printf("\n");
+}
+}
