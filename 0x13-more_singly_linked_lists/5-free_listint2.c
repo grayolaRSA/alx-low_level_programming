12f8e23a6e1f67780a21491dbb70a920901a88ce
grayolaRSA <gtndanga@gmail.com>
2023-02-13 23:14:48 +0200

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,19 +10,18 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *temp2;
+	listint_t *temp, *temp1;
 
 	if (head == NULL)
 	{
 		return;
 	}
-
-	temp = *head;
+	temp = (*head);
 	while (temp != NULL)
 	{
-		temp2 = temp->next;
+		temp1 = temp->next;
 		free(temp);
-		temp = temp2;
+		temp = temp1;
 	}
 	head = NULL;
 }
