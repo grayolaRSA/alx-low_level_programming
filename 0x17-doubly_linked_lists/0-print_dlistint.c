2f842887286e2c6d9f58ab3767cbf7774a30ce6f
grayolaRSA <gtndanga@gmail.com>
2023-03-16 21:54:56 +0200

    move folder to low level programming folder

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - prints all the elements of a list
+ *@h: pointer to the head of the list
+ *
+ *Return: size of list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int c = 0;
+
+	if (h == NULL)
+	{
+		printf("No elements in this list\n");
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - add node at beginning of list
+ *@head: pointer to head pointer
+ *@n: data in node
+ *Return: pointer to new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
+}
