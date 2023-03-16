1c6f628b04f684aca28aa0e895ad51185f9f5425
grayolaRSA <gtndanga@gmail.com>
2023-03-16 22:08:18 +0200

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int size_t = 0;
-	while (h != NULL)
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		size_t++;
 	}
-	return (size_t);
+	return (i);
 }
